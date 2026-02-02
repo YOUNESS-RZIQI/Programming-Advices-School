edb662fd91c4148b56b0f9a13707c64c0fa708e3
Youness Rziqi <rziqiyouness@gmail.com>
2026-02-02 17:50:10 +0100

    Update

---
+++ 1 - All Courses & Practices & Homeworks/Cource 10  OOP Level 1/Video 75: Creat Bank Client Screens/8) Manage Users Screen/5) Dlete User/main.cpp
@@ -0,0 +1,20 @@
+//  ⚗️⚗️⚗️     ⚗️    ⚗️    ⚗️⚗️⚗️     ⚗️    ⚗️   ⚗️   ⚗️
+//  ⚗️         ⚗️⚗️   ⚗️   ⚗️    ⚗️   ⚗️    ⚗️    ⚗️ ⚗️
+//   ⚗️⚗️      ⚗️ ⚗️  ⚗️   ⚗️    ⚗️   ⚗️    ⚗️     ⚗️
+//      ⚗️     ⚗️  ⚗️ ⚗️   ⚗️    ⚗️   ⚗️    ⚗️     ⚗️
+//  ⚗️⚗️⚗️    ⚗️   ⚗️⚗️    ⚗️⚗️⚗️     ⚗️⚗️⚗️    ⚗️
+
+
+
+#include <iostream>
+#include "clsMainScreen.h"
+
+int main()
+{
+
+    clsMainScreen::ShowMainMenue();
+
+
+    system("pause>0");
+    return 0;
+}
