5a35212a5d13eb8d683889fb6133f8fa869cb9fa
snouy <rziqiyouness@gmail.com>
2026-04-20 16:51:05 +0100

    Update

--- 1 - All Courses & Practices & Homeworks/Cource 12 - Data Structures - Level1/5) Stack Data Structure.cpp
+++ 1 - All Courses & Practices & Homeworks/Cource 12 - Data Structures - Level1/5) Stack Data Structure.cpp
@@ -25,3 +25,51 @@ int main(void)
 	return 0;
 
 }
+
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+
+int main(void)
+{
+
+	stack <int> stkNumbers0;
+
+	stkNumbers0.push(50);
+	stkNumbers0.push(40);
+	stkNumbers0.push(30);
+	stkNumbers0.push(20);
+	stkNumbers0.push(10);
+
+	stack <int> stkNumbers1;
+
+	stkNumbers1.push(100);
+	stkNumbers1.push(90);
+	stkNumbers1.push(80);
+	stkNumbers1.push(70);
+	stkNumbers1.push(60);
+
+
+	cout << "stkNumber0:";
+	while (!stkNumbers0.empty())
+	{
+		cout << " " << stkNumbers0.top();
+
+		stkNumbers0.pop();
+	}
+
+	cout << "\nstkNumber1:";
+	while (!stkNumbers1.empty())
+	{
+		cout << " " << stkNumbers1.top();
+
+		stkNumbers1.pop();
+	}
+
+	system("pause>0");
+
+	return 0;
+
+}
