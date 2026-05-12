b5d8898c26505bd6233f9cd15eb9d7c20f738ebd
snouy <rziqiyouness@gmail.com>
2026-05-12 12:41:33 +0100

    Adding 2)

---
+++ 1 - All Courses & Practices & Homeworks/Course 14 - C# Level 1/2) Write Line.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Myclass
+{
+    static void Main(string[] arg)
+    {
+        Console.WriteLine("Hi there This is my Second Line in C# today :)");
+        Console.WriteLine("The Function : Console.WriteLine()");
+        Console.WriteLine("Your C# code\r\n    \nConsole.WriteLine()\r\n    \n.NET runtime libraries\r\n    \nOperating System API\r\n    \nSystem calls\r\n    \nTerminal output\n\n");
+        Console.WriteLine("And In The Write we can do operations like what is 10 + 1327 = (" + (10 + 1327) + ")");
+    }
+}
