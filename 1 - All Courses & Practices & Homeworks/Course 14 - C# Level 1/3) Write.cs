636a59ac8606793789ce0668bc5202d7a08f4624
snouy <rziqiyouness@gmail.com>
2026-05-12 13:06:13 +0100

    adding 4)Formated string

---
+++ 1 - All Courses & Practices & Homeworks/Course 14 - C# Level 1/3) Write.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Myclass
+{
+    static void Main(string[] arg)
+    {
+        Console.WriteLine("The WriteLine Method in C# it write a line with a default new line (\\n)");
+        Console.WriteLine("But Write Method in C# it write a line without a default new line (\\n)");
+    }
+}
---
+++ 1 - All Courses & Practices & Homeworks/Course 14 - C# Level 1/4) Formated_string.cs
@@ -0,0 +1,16 @@
+using System;
+
+class MyCalss
+{
+    internal class Programm
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a Formated String Bellow :");
+            Console.Write("We can Use Formated String in the Method : {0} and {1}\n", "WriteLine()", "Write");
+            Console.WriteLine("This is an Ex:\n");
+            Console.Write("Hello My Name is {0} And My Last Name is {1}\n\n", "YOUNESS", "RZIQI");
+        }
+    }
+
+}
