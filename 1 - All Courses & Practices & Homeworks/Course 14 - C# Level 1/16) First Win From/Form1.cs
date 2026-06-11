f1f3b002fd0db85faf942fea240cfcebc36ed1f2
YOUNESS-RZIQI <younessrziki204@gmail.com>
2026-06-11 15:35:34 +0100

    First Windows From Project: Copy from TextBox to an other

---
+++ 1 - All Courses & Practices & Homeworks/Course 14 - C# Level 1/16) First Win From/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace My_First_Win_Forms_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = textBox2.Text += textBox1.Text;
+        }
+
+        private void button2_MouseEnter(object sender, EventArgs e)
+        {
+            textBox2.Text = textBox2.Text += textBox1.Text;
+        }
+    }
+
+}
