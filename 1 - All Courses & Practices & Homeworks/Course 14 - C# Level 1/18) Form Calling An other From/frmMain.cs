f028e1b699f75666ba805949a79219823b2c0b3d
YOUNESS-RZIQI <younessrziki204@gmail.com>
2026-06-13 15:57:31 +0100

    Main form calling From1

---
+++ 1 - All Courses & Practices & Homeworks/Course 14 - C# Level 1/18) Form Calling An other From/frmMain.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace My_First_Win_Forms_Project
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form frm1 = new Form1();
+            frm1.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           Form frm1 = new Form1();
+            frm1.ShowDialog();
+        }
+    }
+}
