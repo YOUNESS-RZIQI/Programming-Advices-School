84dec200fb4d0d1f4bd342cabb9d542ffcbc7292
YOUNESS-RZIQI <younessrziki204@gmail.com>
2026-08-20 18:44:35 +0100

    Adding 22 dir: Group of New Properties

---
+++ 1 - All Courses & Practices & Homeworks/Course 14 - C# Level 1/22) Group of New Propeties/1) New Properties.md
@@ -0,0 +1,48 @@
+### Check Box Properties:
+
+
+
+1\) PasswordChar: you can choose how to hide the chars
+
+
+
+2\) Read Only: just showing the content of the TextBox
+
+
+
+3\) Max Length: Maximum chars to hold in the TextBox
+
+
+
+4\) Right To Left or Left To Right (default) and Inherit it does get the form cheap, because it is the default (Parent).
+
+
+
+5\) Multy Line: can have newline "\\n"
+
+
+
+6\) Word Wrap: it do a auto "\\n"
+
+
+
+7\) Locked: you can't move the controls.
+
+
+
+8\) Fucus: it get the curser on the text place.
+
+
+
+
+
+### Properties **For All the Controls**:
+
+
+
+1\) Dock: it connect the control with the window it could be full, left, right, upp, down.
+
+
+
+2\) Border Styl.
+
---
+++ 1 - All Courses & Practices & Homeworks/Course 14 - C# Level 1/22) Group of New Propeties/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Tests
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox1.Size = new System.Drawing.Size(224, 450);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.WordWrap = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(493, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 95);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.radioButton1.Location = new System.Drawing.Point(259, 255);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(272, 50);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
